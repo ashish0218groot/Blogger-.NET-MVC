32e5c9ea8787e21022f2a1da5f25f0a850ec1d36
Harsh Kaushal <harshkaushal@gmail.com>
2023-06-08 19:22:07 +0530

    Add project files.

---
+++ Blogger.Web/Data/BloggerDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Blogger.Web.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace Blogger.Web.Data
+{
+    public class BloggerDbContext : DbContext
+    {
+        public BloggerDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<BlogPost> BlogPosts { get; set; }
+        public DbSet<Tag> Tags { get; set; }
+    }
+}
